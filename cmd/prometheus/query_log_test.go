75ee21991e49f1f738a17aa96c21509e607619f4
Julien Pivotto <roidelapluie@inuits.eu>
2020-01-10 23:14:21 +0100

    Add a second test with a prefix
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- cmd/prometheus/query_log_test.go
+++ cmd/prometheus/query_log_test.go
@@ -31,32 +31,37 @@ import (
 // queryLogLine is a basic representation of a query log line.
 type queryLogLine map[string]interface{}
 
-func enableQueryLog(t *testing.T, port int, configFile *os.File, queryLogFile string) {
+type requestOpts struct {
+	port   int
+	prefix string
+}
+
+func enableQueryLog(t *testing.T, opts requestOpts, configFile *os.File, queryLogFile string) {
 	err := configFile.Truncate(0)
 	testutil.Ok(t, err)
 	_, err = configFile.Seek(0, 0)
 	testutil.Ok(t, err)
 	_, err = configFile.Write([]byte(fmt.Sprintf("global:\n  query_log_file: %s\n", queryLogFile)))
 	testutil.Ok(t, err)
-	postReloadConfig(t, port)
+	postReloadConfig(t, opts)
 }
 
-func postReloadConfig(t *testing.T, port int) {
-	r, err := http.Post(fmt.Sprintf("http://127.0.0.1:%d/-/reload", port), "text/plain", nil)
+func postReloadConfig(t *testing.T, opts requestOpts) {
+	r, err := http.Post(fmt.Sprintf("http://127.0.0.1:%d%s/-/reload", opts.port, opts.prefix), "text/plain", nil)
 	testutil.Ok(t, err)
 	testutil.Equals(t, 200, r.StatusCode)
 }
 
-func disableQueryLog(t *testing.T, port int, configFile *os.File) {
+func disableQueryLog(t *testing.T, opts requestOpts, configFile *os.File) {
 	err := configFile.Truncate(0)
 	testutil.Ok(t, err)
-	postReloadConfig(t, port)
+	postReloadConfig(t, opts)
 }
 
 type queryOpts struct {
 	useIPV6 bool
 	query   string
-	port    int
+	opts    requestOpts
 }
 
 func query(t *testing.T, o queryOpts) {
@@ -65,9 +70,10 @@ func query(t *testing.T, o queryOpts) {
 		host = "[::1]"
 	}
 	_, err := http.Get(fmt.Sprintf(
-		"http://%s:%d/api/v1/query?query=%s",
+		"http://%s:%d%s/api/v1/query?query=%s",
 		host,
-		o.port,
+		o.opts.port,
+		o.opts.prefix,
 		url.QueryEscape(o.query),
 	))
 	testutil.Ok(t, err)
@@ -86,10 +92,12 @@ func readQueryLog(t *testing.T, path string) []queryLogLine {
 	}
 	return ql
 }
-func TestQueryLog(t *testing.T) {
+
+func TestQueryLog_basic(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	}
+
 	port := 9091
 
 	queryLogFile, err := ioutil.TempFile("", "query")
@@ -99,6 +107,10 @@ func TestQueryLog(t *testing.T) {
 	testutil.Ok(t, err)
 	defer os.Remove(queryLogFile.Name())
 
+	opts := requestOpts{
+		port: port,
+	}
+
 	prom := exec.Command(promPath, "--config.file="+configFile.Name(), "--web.enable-lifecycle", fmt.Sprintf("--web.listen-address=0.0.0.0:%d", port))
 	testutil.Ok(t, prom.Start())
 	defer func() {
@@ -106,12 +118,12 @@ func TestQueryLog(t *testing.T) {
 		prom.Wait()
 	}()
 
-	testutil.Ok(t, waitForPrometheus(port))
+	testutil.Ok(t, waitForPrometheus(opts))
 
-	enableQueryLog(t, port, configFile, queryLogFile.Name())
+	enableQueryLog(t, opts, configFile, queryLogFile.Name())
 	testutil.Equals(t, 0, len(readQueryLog(t, queryLogFile.Name())))
 
-	query(t, queryOpts{port: port, query: "time()"})
+	query(t, queryOpts{opts: opts, query: "time()"})
 	ql := readQueryLog(t, queryLogFile.Name())
 	testutil.Equals(t, 1, len(ql))
 	testutil.Equals(t, ql[0]["query"].(string), "time()")
@@ -119,19 +131,19 @@ func TestQueryLog(t *testing.T) {
 	testutil.Equals(t, ql[0]["path"].(string), "/api/v1/query")
 	testutil.Equals(t, ql[0]["method"].(string), "GET")
 
-	query(t, queryOpts{port: port, query: "vector(1) ", useIPV6: true})
+	query(t, queryOpts{opts: opts, query: "vector(1) ", useIPV6: true})
 	ql = readQueryLog(t, queryLogFile.Name())
 	testutil.Equals(t, 2, len(ql))
 	testutil.Equals(t, ql[1]["query"].(string), "vector(1) ")
 	testutil.Equals(t, ql[1]["clientIP"].(string), "::1")
 
-	disableQueryLog(t, port, configFile)
-	query(t, queryOpts{port: port, query: "time()"})
+	disableQueryLog(t, opts, configFile)
+	query(t, queryOpts{opts: opts, query: "time()"})
 	ql = readQueryLog(t, queryLogFile.Name())
 	testutil.Equals(t, 2, len(ql))
 
-	enableQueryLog(t, port, configFile, queryLogFile.Name())
-	query(t, queryOpts{port: port, query: "time()"})
+	enableQueryLog(t, opts, configFile, queryLogFile.Name())
+	query(t, queryOpts{opts: opts, query: "time()"})
 	ql = readQueryLog(t, queryLogFile.Name())
 	testutil.Equals(t, 3, len(ql))
 
@@ -143,25 +155,64 @@ func TestQueryLog(t *testing.T) {
 	ql = readQueryLog(t, newFile.Name())
 	testutil.Equals(t, 3, len(ql))
 
-	query(t, queryOpts{port: port, query: "time()"})
+	query(t, queryOpts{opts: opts, query: "time()"})
 	testutil.Ok(t, err)
 	ql = readQueryLog(t, newFile.Name())
 	testutil.Equals(t, 4, len(ql))
 
 	// Reload config, Prometheus should write to the new file..
-	postReloadConfig(t, port)
-	query(t, queryOpts{port: port, query: "time()"})
+	postReloadConfig(t, opts)
+	query(t, queryOpts{opts: opts, query: "time()"})
 	ql = readQueryLog(t, newFile.Name())
 	testutil.Equals(t, 4, len(ql))
 	ql = readQueryLog(t, queryLogFile.Name())
 	testutil.Equals(t, 1, len(ql))
 }
 
-func waitForPrometheus(port int) error {
+func TestQueryLog_CustomPrefix(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	port := 9092
+
+	queryLogFile, err := ioutil.TempFile("", "query")
+	testutil.Ok(t, err)
+	defer os.Remove(queryLogFile.Name())
+	configFile, err := ioutil.TempFile("", "config")
+	testutil.Ok(t, err)
+	defer os.Remove(queryLogFile.Name())
+
+	opts := requestOpts{
+		prefix: "/test",
+		port:   port,
+	}
+
+	prom := exec.Command(promPath, "--config.file="+configFile.Name(), "--web.enable-lifecycle", fmt.Sprintf("--web.listen-address=0.0.0.0:%d", port), "--web.route-prefix=/test")
+	testutil.Ok(t, prom.Start())
+	defer func() {
+		prom.Process.Signal(os.Interrupt)
+		prom.Wait()
+	}()
+
+	testutil.Ok(t, waitForPrometheus(opts))
+
+	enableQueryLog(t, opts, configFile, queryLogFile.Name())
+	testutil.Equals(t, 0, len(readQueryLog(t, queryLogFile.Name())))
+
+	query(t, queryOpts{opts: opts, query: "time()"})
+	ql := readQueryLog(t, queryLogFile.Name())
+	testutil.Equals(t, 1, len(ql))
+	testutil.Equals(t, ql[0]["query"].(string), "time()")
+	testutil.Equals(t, ql[0]["clientIP"].(string), "127.0.0.1")
+	testutil.Equals(t, ql[0]["path"].(string), "/test/api/v1/query")
+	testutil.Equals(t, ql[0]["method"].(string), "GET")
+}
+
+func waitForPrometheus(opts requestOpts) error {
 	var err error
 	for x := 0; x < 10; x++ {
 		// error=nil means prometheus has started so can send the interrupt signal and wait for the grace shutdown.
-		if _, err = http.Get(fmt.Sprintf("http://localhost:%d/graph", port)); err == nil {
+		if _, err = http.Get(fmt.Sprintf("http://localhost:%d%s/graph", opts.port, opts.prefix)); err == nil {
 			break
 		}
 		time.Sleep(500 * time.Millisecond)
