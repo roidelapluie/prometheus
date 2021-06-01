6263b7bb7968ee15b43b978ddd61e8e117811bda
Julien Pivotto <roidelapluie@inuits.eu>
2021-06-01 14:04:58 +0200

    Add PuppetDB service discovery
    We have been Puppet user for 10 years and we are users of
    https://github.com/camptocamp/prometheus-puppetdb-sd
    
    However, that file_sd implementation contains business logic and
    assumptions around e.g. the modules which you are using.
    
    This pull request adds a simple PuppetDB service discovery, which will
    enable more use cases than the upstream sd.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- discovery/install/install.go
+++ discovery/install/install.go
@@ -30,6 +30,7 @@ import (
 	_ "github.com/prometheus/prometheus/discovery/marathon"     // register marathon
 	_ "github.com/prometheus/prometheus/discovery/moby"         // register moby
 	_ "github.com/prometheus/prometheus/discovery/openstack"    // register openstack
+	_ "github.com/prometheus/prometheus/discovery/puppetdb"     // register puppetdb
 	_ "github.com/prometheus/prometheus/discovery/scaleway"     // register scaleway
 	_ "github.com/prometheus/prometheus/discovery/triton"       // register triton
 	_ "github.com/prometheus/prometheus/discovery/zookeeper"    // register zookeeper
